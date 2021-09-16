d10d1e4765c65d9e459ed18acd687ee217aaec41
javierlopezg00 <javierlopezguzman00@gmail.com>
2021-09-16 13:23:32 -0600

    line2FirstCommit

--- src/apps.js
+++ src/apps.js
@@ -1,11 +1,11 @@
 var juego1 ={
-    name: "Minecraft",
+    name: "GTA V",
     img: "../../src/img/1.jpg",
     description :"Prepare for an adventure of limitless possibilities as you build, mine, battle mobs, and explore the ever-changing Minecraft landscape."
 };
 
 var juego2 ={
-    name: "Valorant",
+    name: "Tetris",
     img: "../../src/img/2.jpg",
     description :"Prepare for an adventure of limitless possibilities as you build, mine, battle mobs, and explore the ever-changing Minecraft landscape."
 };
